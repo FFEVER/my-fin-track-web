be77dc79e0eada7940393c365a0f51d8fb5ff90c
nattapho.srisa <rootncdev2015@gmail.com>
2023-04-23 18:25:41 +0700

    Add favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,6 +19,7 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
